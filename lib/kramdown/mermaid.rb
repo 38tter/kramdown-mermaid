527ee0f992c7d4e8fbf74f741d4215c11cc80e3d
Seiya Miyata <odradek38@gmail.com>
2023-06-10 02:01:02 +0900

    Run Creating gem 'kramdown-mermaid'... MIT License enabled in config Code of conduct enabled in config Changelog enabled in config RuboCop enabled in config Initializing git repo in /Users/s.miyata/wd/38tter/kramdown-mermaid/kramdown-mermaid       create  kramdown-mermaid/Gemfile       create  kramdown-mermaid/lib/kramdown/mermaid.rb       create  kramdown-mermaid/lib/kramdown/mermaid/version.rb       create  kramdown-mermaid/sig/kramdown/mermaid.rbs       create  kramdown-mermaid/kramdown-mermaid.gemspec       create  kramdown-mermaid/Rakefile       create  kramdown-mermaid/README.md       create  kramdown-mermaid/bin/console       create  kramdown-mermaid/bin/setup       create  kramdown-mermaid/.gitignore       create  kramdown-mermaid/.rspec       create  kramdown-mermaid/spec/spec_helper.rb       create  kramdown-mermaid/spec/kramdown/mermaid_spec.rb       create  kramdown-mermaid/.github/workflows/main.yml       create  kramdown-mermaid/LICENSE.txt       create  kramdown-mermaid/CODE_OF_CONDUCT.md       create  kramdown-mermaid/CHANGELOG.md       create  kramdown-mermaid/.rubocop.yml Gem 'kramdown-mermaid' was successfully created. For more information on making a RubyGem visit https://bundler.io/guides/creating_gem.html

---
+++ lib/kramdown/mermaid.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "mermaid/version"
+
+module Kramdown
+  module Mermaid
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/kramdown/mermaid_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Kramdown::Mermaid do
+  it "has a version number" do
+    expect(Kramdown::Mermaid::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
