234927a66a6d707350d9fde418954b722b4ca324
Seiya Miyata <odradek38@gmail.com>
2023-06-10 03:16:11 +0900

    Add monkey patch category

--- lib/kramdown-mermaid.rb
+++ lib/kramdown-mermaid.rb
@@ -5,8 +5,7 @@ require_relative "kramdown-mermaid/version"
 require 'kramdown'
 
 module Kramdown
-  module Mermaid
-    class Error < StandardError; end
-    # Your code goes here...
+  class Element
+    CATEGORY[:er_diagram] = :block
   end
 end
