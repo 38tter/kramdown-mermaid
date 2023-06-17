cee7e01eab86f0d9edf6426518a15444fefa94cf
Seiya Miyata <odradek38@gmail.com>
2023-06-17 17:01:09 +0900

    Fix test

--- spec/kramdown-mermaid_spec.rb
+++ spec/kramdown-mermaid_spec.rb
@@ -1,11 +1,7 @@
 # frozen_string_literal: true
 
-RSpec.describe Kramdown::Parser::Mermaid do
+RSpec.describe Kramdown::Parser do
   it 'has a version number' do
     expect(Kramdown::Parser::KRAMDOWN_MERMAID_VERSION).not_to be nil
-  end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
   end
 end
